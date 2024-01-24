3cfb3af3ef169c6f0fee07b23372e8cae5cefcf2
jonathaLouder <jonatha@mylouder.com>
2024-01-23 21:25:14 -0300

    updates from improvements

--- src/App.js
+++ src/App.js
@@ -1,96 +1,141 @@
-
-import './App.css';
 import React, { useEffect, useState } from 'react';
-import {Table} from 'reactstrap'
-import { HiArchiveBoxXMark } from "react-icons/hi2";
-import { HiOutlinePencil } from "react-icons/hi2";
+import { Table, Modal, ModalBody, ModalHeader, Button, Input } from 'reactstrap';
+import { HiArchiveBoxXMark, HiOutlinePencil  } from "react-icons/hi2";
 import FormCreate from './components/Form';
+import { CiSearch } from "react-icons/ci";
 import axios from 'axios';
-
-const arrayAll = [
-  {
-    name:"jonatha",
-    email:"jonatha@teste.com.br"
-  }
-]
-const AllClient = ({allClient}) =>{
-  return(
-     <div className="allClient">
-       {allClient.map((clients)=>{
-         return(
-          
-
-<div className='Table'>
- <Table hover>
-  <thead>
-    <tr>
-      <th>
-        #
-      </th>
-      <th>
-        Name
-      </th>
-      <th>
-        Email
-      </th>
-      <th>
-        Action
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">
-        1
-      </th>
-      <td>
-       {clients.name}
-      </td>
-      <td>
-    {clients.email}
-      </td>
-      <td>
-        <button><HiArchiveBoxXMark /></button>
-        <button><HiOutlinePencil /></button>
-      </td>
-    </tr>
-   
-  </tbody>
-   </Table>
-   </div>
- 
-         )
-       })}
-        
-     </div>
-  )
-}
-
+import './App.css';
 
 function App() {
-   
   const [todos, setTodos] = useState([]);
+  const [editingClient, setEditingClient] = useState(null);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [nameFilter, setNameFilter] = useState('');
 
   async function getTodos() {
     const response = await axios.get("http://localhost:3333/todos");
     setTodos(response.data);
-    console.log(response.data);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getTodos();
-  },[])
+  }, []);
 
+  async function deletetodo(client) {
+    await axios.delete(`http://localhost:3333/todos/${client.id}`);
+    getTodos();
+  }
 
+  const openEditModal = (client) => {
+    setEditingClient(client);
+    setEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditingClient(null);
+    setEditModalOpen(false);
+  };
+
+  const handleUpdate = async () => {
+    if (editingClient) {
+      await axios.put(`http://localhost:3333/todos`, {
+        id: editingClient.id,
+        name: editingClient.name,
+        email: editingClient.email,
+        telefone: editingClient.telefone
+      });
+      closeEditModal();
+      getTodos();
+    }
+  };
+
+  const filterByName = (client) => {
+    return client.name.toLowerCase().includes(nameFilter.toLowerCase());
+  };
+
+  const filteredClients = todos.filter(filterByName);
+
+  const AllClient = ({ allClient }) => {
+    return (
+      <div className="allClient">
+       
+        <div className='Table'>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredClients.map((clients) => (
+                <tr key={clients.id}>
+                  <th scope="row">{clients.id}</th>
+                  <td>{clients.name}</td>
+                  <td>{clients.email}</td>
+                  <td>{clients.telefone}</td>
+                  <td>
+                    <button onClick={() => deletetodo(clients)}>
+                      <HiArchiveBoxXMark />
+                    </button>
+                    <button onClick={() => openEditModal(clients)}>
+                      <HiOutlinePencil />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <FormCreate/>
+          <FormCreate />
         </div>
-      <AllClient allClient={arrayAll}/>
-    
+
+      <div className='SearchComponent'>
+        <Input
+          type="search"
+          placeholder="Filter by name"
+          value={nameFilter}
+          onChange={(e) => setNameFilter(e.target.value)}
+          className='filterTable'
+        /><CiSearch/>
+        </div>
+        <AllClient allClient={todos} />
+        <Modal isOpen={isEditModalOpen} toggle={closeEditModal}>
+          <ModalHeader toggle={closeEditModal}>Edit Client</ModalHeader>
+          <ModalBody>
+            <div className='ModalEdit'>
+              <input
+                type="text"
+                value={editingClient ? editingClient.name : ''}
+                onChange={(e) => setEditingClient({ ...editingClient, name: e.target.value })}
+              />
+              <input
+                type="text"
+                value={editingClient ? editingClient.email : ''}
+                onChange={(e) => setEditingClient({ ...editingClient, email: e.target.value })}
+              />
+              <input
+                type="text"
+                value={editingClient ? editingClient.telefone : ''}
+                onChange={(e) => setEditingClient({ ...editingClient, telefone: e.target.value })}
+              />
+              <Button color="primary" onClick={handleUpdate}>
+                Save Changes
+              </Button>
+            </div>
+          </ModalBody>
+        </Modal>
       </header>
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,56 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import App from '../App';
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
 function FormCreate(args) {
   const [modal, setModal] = useState(false);
+  const [nameValue, setNameValue] = useState();
+  const [emailValue, setEmailValue] = useState();
+  const [teleValue, setTeleValue] = useState();
 
   const toggle = () => setModal(!modal);
+  async function CreateTodo() {
+    const response = await axios.post("http://localhost:3333/todos", {
+      name: nameValue,
+      email: emailValue,
+      telefone:teleValue,
+    });
 
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+  }
   return (
     <div>
-      <Button color="danger" onClick={toggle}>
-        Click Me
+      <Button className="btnAdd" onClick={toggle}>
+      Cadaster +
       </Button>
       <Modal isOpen={modal} toggle={toggle} {...args}>
         <ModalHeader toggle={toggle}>+ New Client</ModalHeader>
-        <ModalBody className='form'>
-          <label>Name:</label>  
-          <input type='text'></input>
-          <label>email:</label>  
-          <input type='text'></input>
+        <ModalBody className="form">
+          <label>Name:</label>
+          <input
+            type="text"
+            className="inputName"
+            onChange={(e) => setNameValue(e.target.value)}
+          ></input>
+          <label>email:</label>
+          <input
+            type="text"
+            className="inputEmail"
+            onChange={(e) => setEmailValue(e.target.value)}
+          ></input>
+          <label >Telefone</label>
+           <input
+            type="text"
+            className="inputTele"
+            onChange={(e) => setTeleValue(e.target.value)}
+          ></input>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-           + Create
-          </Button>{' '}
-         
+          <Button color="primary" onClick={CreateTodo}>
+            + Create
+          </Button>
         </ModalFooter>
       </Modal>
     </div>
